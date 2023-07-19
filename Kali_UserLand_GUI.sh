2faac04cbad23d1d9f2280cc30e39af6fad642bc
Kareem Ebrahem <90919499+mrx7014@users.noreply.github.com>
2023-07-19 15:50:17 +0300

    Fix some errors

--- Kali_UserLand_GUI.sh
+++ Kali_UserLand_GUI.sh
@@ -14,23 +14,24 @@ echo -e "\e[93mSetup Starting Now\e[0m"
 sleep 1
 echo " "
 echo " "
-#Get the necessary components
+echo -e "\e[93mGet the necessary components\e[0m"
 sudo apt-get update
 sudo apt-get full-upgrade -y
 sudo apt-get install xfce4 xfce4-terminal tightvncserver dbus-x11 -y
 sudo apt-get install xfe -y
-sudo apt-get kali-themes
+sudo apt-get install -y kali-themes
 sudo apt-get install -y kali-linux-default
 sudo apt-get clean
-
-#Install Brave Browser
+sleep 1
+echo -e "\e[93mInstall Brave Browser\e[0m"
+sleep 1
 sudo apt install curl
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt update
 sudo apt install brave-browser
 
-#Setup the necessary files
+echo -e "\e[93mSetup the necessary VNC files\e[0m"
 mkdir ~/.vnc
 wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/DesktopEnvironment/Apt/Xfce4/xstartup --no-check-certificate -P ~/.vnc/
 wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/DesktopEnvironment/Apt/Xfce4/vncserver-start --no-check-certificate -P /usr/local/bin/
@@ -41,29 +42,29 @@ chmod +x /usr/local/bin/vncserver-start
 chmod +x /usr/local/bin/vncserver-stop
 
 echo " "
-echo "You can now start vncserver by running vncserver-start"
+echo -e "\e[93mYou can now start vncserver by running vncserver-start\e[0m"
 sleep 2
 echo " "
-echo "It will ask you to enter a password when first time starting it."
+echo -e "\e[93mIt will ask you to enter a password when first time starting it.\e[0m"
 echo " "
 sleep 2
-echo "The VNC Server will be started at Localhost:1"
+echo -e "\e[93mThe VNC Server will be started at localhost:1\e[0m"
 echo " "
 sleep 2
-echo "You can connect to this address with a VNC Viewer you prefer"
+echo -e "\e[93mYou can connect to this address with a VNC Viewer you prefer\e[0m"
 echo " "
 sleep 2
-echo "Connect to this address will open a window with Xfce4 Desktop Environment with Kali Theme"
-echo " "
-echo " "
-echo " "
-sleep 2
-echo "Running vncserver-start"
+echo -e "\e[93mConnect to this address will open a window with Xfce4 Desktop Environment with Kali Theme\e[0m"
 echo " "
 echo " "
 echo " "
 sleep 2
-echo "To Kill VNC Server just run vncserver-stop"
+echo -e "\e[93mRunning vncserver-start\e[0m"
+echo " "
+echo " "
+echo " "
+sleep 2
+echo -e "\e[93mTo Kill VNC Server just run vncserver-stop\e[0m"
 echo " "
 echo " "
 echo " "
